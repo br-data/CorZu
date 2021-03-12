a4d0f38ba89add8bd9a93beb9ce08d032fa04e1a
Marco Lehner <marco-lehner@posteo.net>
2021-03-12 17:28:13 +0100

    :whale: get parzu from local docker image

---
+++ corzu_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+corzu_dir=$(dirname "$SCRIPT")
+parzu_url="http://127.0.0.1:5003"
+parzu_cmd="parzu -q"   #the command used for starting parzu
+
+echo "Parsing ..."
+echo '{"text": "'$(cat $1)'"}' | curl -H "Content-Type: application/json" -X POST -d @- "$parzu_url/parse/" > $1.parzu
+#cat $1 |$parzu_cmd > $1".parzu" #parse
+echo "\nDone.\nExtracting markables..."
+python $corzu_dir/extract_mables_from_parzu.py $1".parzu" > $1".mables" #markable extraction
+echo "Done.\nResolving coreference..."
+python $corzu_dir/corzu.py $1".mables" $1".parzu" $1".coref"  #coreference resolution
+echo "\nDone.\nWriting HTML..."
+python $corzu_dir/conll_to_html.py $1".coref" > $1".html"
+echo "\nDone."
+#rm $1".mables" #uncomment to delete markable file
+#rm $1".parzu" #uncomment to delete parse file
